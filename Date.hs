d6ffed4837c49dced37ce45f9be8520ff3bb0dac
Nick Straw <nick@broxholme.com>
2012-11-05 21:58:53 +0000

    First build

---
+++ Date.hs
@@ -0,0 +1,36 @@
+
+module Date
+    (
+        clock2UTC
+        , ftpInt2UTC
+    ) where
+
+import System.Time
+import Data.Time.Calendar (fromGregorian)
+import Data.Time.Clock
+
+clock2UTC::ClockTime->UTCTime
+clock2UTC clktm = UTCTime (fromGregorian yy mm dd) dt
+                where
+                    caltm = toUTCTime clktm
+                    yy = toInteger $ ctYear caltm
+                    mm = fromEnum $ ctMonth caltm
+                    dd = ctDay caltm
+                    hh = toInteger $ ctHour caltm
+                    mmm = toInteger $ ctMin caltm
+                    ss = toInteger $ ctSec caltm
+                    dt = secondsToDiffTime $ ((hh*60+mmm)*60)+ss
+
+ftpInt2UTC::Integer->UTCTime
+ftpInt2UTC fint = UTCTime gday dt
+    where
+        yy = floor $ (fromInteger fint)/10000000000
+        mm = (floor $ (fromInteger fint)/100000000) - yy*100
+        dd = (floor $ (fromInteger fint)/1000000) - yy*10000 - mm*100
+        hh = (floor $ (fromInteger fint)/10000) - yy*1000000 - mm*10000 - dd*100
+        ms = (floor $ (fromInteger fint)/100) - yy*100000000 - mm*1000000 - dd*10000 - hh*100
+        ss = (floor $ (fromInteger fint)) - yy*10000000000 - mm*100000000 - dd*1000000 - hh*10000 - ms*100
+        gday = fromGregorian yy (fromInteger mm) (fromInteger dd)
+        dt = secondsToDiffTime $ (hh*60 + ms)*60+ss
+                    
+                    
---
+++ dist/build/autogen/Paths_date.hs
@@ -0,0 +1,32 @@
+module Paths_date (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+
+version :: Version
+version = Version {versionBranch = [0,0], versionTags = []}
+bindir, libdir, datadir, libexecdir :: FilePath
+
+bindir     = "c:/Documents and Settings/Robin Seeley/workspace/bin\\bin"
+libdir     = "c:/Documents and Settings/Robin Seeley/workspace/bin\\date-0.0\\ghc-7.4.1"
+datadir    = "c:/Documents and Settings/Robin Seeley/workspace/bin\\date-0.0"
+libexecdir = "c:/Documents and Settings/Robin Seeley/workspace/bin\\date-0.0"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir = catchIO (getEnv "date_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "date_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "date_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "date_libexecdir") (\_ -> return libexecdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
